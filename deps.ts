758bd911c98e93c92eabb8c94f58bb1f186ef638
Gabriel <gabrielmunumel@gmail.com>
2022-08-02 14:53:41 +0200

    first commit

---
+++ deps.ts
@@ -0,0 +1,11 @@
+export {
+  type InnerRenderFunction,
+  RenderContext,
+  start,
+} from "$fresh/server.ts";
+import dev from "$fresh/dev.ts";
+
+export { h } from "preact";
+export { useState } from "preact/hooks";
+
+export { dev };
---
+++ routes/[name].tsx
@@ -0,0 +1,8 @@
+/** @jsx h */
+// import { h } from "preact";
+import { h } from "../deps.ts";
+import { PageProps } from "$fresh/server.ts";
+
+export default function Greet(props: PageProps) {
+  return <div>Hello {props.params.name}</div>;
+}
